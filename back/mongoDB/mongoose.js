78d2d893521a765dfb1a2e1e525342441d73938e
Jan Jaap Braam <jjbraam@gmail.com>
2019-05-27 16:12:43 +0200

    create and find files mongoDB cloud now works

--- back/mongoDB/mongoose.js
+++ back/mongoDB/mongoose.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose')
 
-const SECRETS = require('./secrets.json')
+const SECRETS = require('../secrets.json')
 
-const mongodb_uri = `mongodb+srv://${SECRETS.ATLAS_USERNAME}:${SECRETS.ATLAS_PASSWORD}@cluster0-fubvg.mongodb.net/test?retryWrites=true`
+const mongodb_uri = `mongodb+srv://${SECRETS.ATLAS_USERNAME}:${SECRETS.ATLAS_PASSWORD}@cluster0-fubvg.mongodb.net/e-commerce-mern?retryWrites=true`
 
 mongoose.connect(mongodb_uri, { useNewUrlParser: true });
-const connection = mongoose.connection;
+module.exports = mongoose.connection;
 
-connection.once('open', function() {
-    console.log("MongoDB database connection established successfully");
-})

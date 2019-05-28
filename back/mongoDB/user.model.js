162fa94f6e839f82a14098c06387ff255541cb37
Thomas Neil <12561498+tn819@users.noreply.github.com>
2019-05-28 14:18:57 +0200

    adding passport local strategy to mongo

--- back/mongoDB/user.model.js
+++ back/mongoDB/user.model.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-let userSchema = new Schema({
+//note: username and password do not need to be in schema to work
+
+let User = new Schema({
     email: String,
     name: {
       first: String,
       last: String,
-    },
-    created_at: { type: Date, default: Date.now }
+    }
 });
 
-module.exports = mongoose.model('User', userSchema);
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', User);
